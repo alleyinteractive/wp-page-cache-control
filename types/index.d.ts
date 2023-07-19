745c7f9a1675f47a7188507a35f2411915bca890
Sean Fisher <srtfisher@gmail.com>
2023-07-19 15:09:29 -0400

    Define functions for the namespace

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,6 +4,31 @@
  * @link https://github.com/alleyinteractive/wp-page-cache-control
  */
 declare namespace wpPageCacheControl {
+  /**
+   * Cache groups for the current user.
+   */
+  const groups: Record<string, string>;
+
+  /**
+   * Method called when the cache groups should be read/updated.
+   */
+  function read(): void;
+
+  /**
+   * Check if the current user is in a cache group.
+   */
+  function isUserInGroup(group: string): boolean;
+
+  /**
+   * Check if the current user is in a cache group segment.
+   */
+  function isUserInGroupSegment(group: string, segment: string): boolean;
+
+  /**
+   * Set the cache group segment for the current user.
+   */
+  function setGroupForUser(group: string, segment: string): boolean;
+
   interface WPPageCacheControlProvider {
     /**
      * Cache groups for the current user.
